98a46facc0348f902ff1ed6759d58240b46d8c34
SANJAY K SAI <shachsan@hotmail.com>
2018-10-14 04:11:24 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -13,21 +13,21 @@ def second_supply_for_fourth_of_july(holiday_hash)
 end
 
   # given that holiday_hash looks like this:
-  # h = {
-  #   :winter => {
-  #     :christmas => ["Lights", "Wreath"],
-  #     :new_years => ["Party Hats"]
-  #   },
-  #   :summer => {
-  #     :fourth_of_july => ["Fireworks", "BBQ"]
-  #   },
-  #   :fall => {
-  #     :thanksgiving => ["Turkey"]
-  #   },
-  #   :spring => {
-  #     :memorial_day => ["BBQ"]
-  #   }
-  # }
+  h = {
+    :winter => {
+      :christmas => ["Lights", "Wreath"],
+      :new_years => ["Party Hats"]
+    },
+    :summer => {
+      :fourth_of_july => ["Fireworks", "BBQ"]
+    },
+    :fall => {
+      :thanksgiving => ["Turkey"]
+    },
+    :spring => {
+      :memorial_day => ["BBQ"]
+    }
+  }
   # return the second element in the 4th of July array
 
 
@@ -78,7 +78,7 @@ def all_winter_holiday_supplies(holiday_hash)
       end
     end
 end
-
+all_winter_holiday_supplies
 def all_supplies_in_holidays(holiday_hash)
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
