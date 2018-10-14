5501fa2a167ab12611ed94a6cfde5ae2269ca868
SANJAY K SAI <shachsan@hotmail.com>
2018-10-14 03:06:19 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,6 +1,7 @@
 require 'pry'
 
 def second_supply_for_fourth_of_july(holiday_hash)
+  return holiday_hash.dig(:summer, :fourth_of_july)
   # given that holiday_hash looks like this:
   # {
   #   :winter => {
@@ -61,10 +62,3 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
 
 end
-
-
-
-
-
-
-
